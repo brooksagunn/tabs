a22fd34da34c75c50ba1d5887e38dee0c941259a
Brooks Gunn <brooks.a.gunn@gmail.com>
2024-01-03 16:29:34 -0500

    Add active tab functionality

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
-document.querySelector("");
+let tabs = document.querySelectorAll(".tab");
+
+for (let i = 0; i < tabs.length; i++) {
+  tabs[i].addEventListener("click", (e) => {
+    for (tab of tabs) {
+      tab.classList.remove("isActive");
+      console.log(tab);
+    }
+    tabs[i].classList.add("isActive");
+  });
+}
